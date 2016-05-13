2f0467593b5307c53d0ad5cff6985b6ffbae15da
Ross Wilson <ross.wilson@bbc.co.uk>
2016-05-13 14:58:16 +0100

    Server script now uses Express and Mustache templates to render the application bootstrap HTML

--- index.js
+++ index.js
@@ -24,166 +24,57 @@
  * Please contact us for an alternative licence
  */
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 var AntieFramework = require('tal');
+var mustacheExpress = require('mustache-express');
 
-// INIT AND CONFIG LOAD
-function returnResponse(url, response)
-{
-	// Set up application ID and path to framework configuration directory
-	var application_id = "sampleapp";
-	var configPath = "node_modules/tal/config";
+// Setup mustache for view templating
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
-	var antie = new AntieFramework(configPath);
+app.get('/', function (req, res) {
 
-	// Get brand and model from url parameters, or use
-	// brand = default, model = webkit
-	var device_brand = "default";
-	var device_model = "webkit";
+  // Path to device configuration directory
+  var configPath = "node_modules/tal/config";
+  var antie = new AntieFramework(configPath);
 
-	// Split the url in order to get the brand and model (if applicable)
-	if (url.search)
-	{
-		var parts = url.search.replace(/^\?/, '').split("&");
-		var $_GET = {};
-		for (var i = 0; i < parts.length; i++) {
-			var temp = parts[i].split("=");
-			$_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
-		}
+  // Get normalised brand and model from url parameters
+  var device_brand = antie.normaliseKeyNames(req.query.brand || "default");
+  var device_model = antie.normaliseKeyNames(req.query.model || "webkit");
 
-		if ($_GET.brand) {
-			device_brand = $_GET.brand;
-		}
-		if ($_GET.model) {
-			device_model = $_GET.model;
-		}
-	}
+  // Load framework device config files, named BRAND-MODEL-default.json
+  var device_configuration;
 
+  try {
+    device_configuration = antie.getConfigurationFromFilesystem(device_brand + "-" + device_model + "-default", "/devices");
+  } catch(e) {
+    res.status(406).render('error', {
+      exception: e
+    });
 
-	// Normalises to lower case with spaces replaced by underscores
-	device_brand = antie.normaliseKeyNames(device_brand);
-	device_model = antie.normaliseKeyNames(device_model);
+    return;
+  }
 
-	// Framework device config files in format BRAND-MODEL-default.json
-	// Construct filename from this and config path
-	var device_configuration_name = device_brand + "-" + device_model;
-	var device_configuration_file_path = "/devices";
-	var device_configuration;
+  // Substitute application_id wherever /%application%/ token is present in device configuration
+  var application_id = "sampleapp";
+  device_configuration = device_configuration.replace(/%application%/g, application_id);
 
-	// Load in device configuration
-	try
-	{
-		device_configuration = antie.getConfigurationFromFilesystem(device_configuration_name + "-default", device_configuration_file_path);
-	}
-	catch(e)
-	{
-		response.write("Device configuration not supported: " + e);
-		return;
-	}
+  var device_configuration_decoded = JSON.parse(device_configuration);
 
-	// Substitute appid wherever /%application%/ is present in device configuration
-	device_configuration = device_configuration.replace(/%application%/g, application_id);
+  res.render('index', {
+    root_html_tag: antie.getRootHtmlTag(device_configuration_decoded),
+    headers: antie.getDeviceHeaders(device_configuration_decoded),
+    application_id: application_id,
+    device_configuration: device_configuration,
+    extra_body: antie.getDeviceBody(device_configuration_decoded)
+  });
+});
 
-	// Decode to php object
-	var device_configuration_decoded = JSON.parse(device_configuration);
+app.use('/tal', express.static('node_modules/tal'));
+app.use('/static', express.static('static'));
 
-	// PAGE GENERATION
-
-	response.write("<head>");
-	response.write(antie.getDeviceHeaders(device_configuration_decoded));
-
-	response.write("<script type='text/javascript'>");
-			response.write("var require = {");
-				response.write("baseUrl: '',");
-				response.write("paths: {");
-					response.write(application_id + ": 'static/script',");
-					response.write("antie : 'node_modules/tal/static/script'");
-				response.write("},");
-				response.write("priority: [],");
-				response.write("callback: function() {}");
-			response.write("};");
-		response.write("</script>");
-		response.write("<script type='text/javascript' src='./node_modules/tal/static/script/lib/require.js'></script>");
-		response.write("<link rel='stylesheet' href='static/style/base.css'/>");
-
-		response.write("<script>");
-			response.write("var antie = {framework: { deviceConfiguration: " + device_configuration + "}}");
-		response.write("</script>");
-
-	response.write("</head>");
-
-	response.write("<body style='background: #000;'>");
-	antie.getDeviceBody(device_configuration_decoded);
-
-	response.write("<div id='static-loading-screen' style='position: absolute; width: 100%; height: 100%; background: #000;'>");
-	response.write("Application is loading...");
-	response.write("</div>");
-
-	response.write("<div id=\"app\" class=\"display-none\"><\/div>");
-
-	response.write("<script type='text/javascript'>");
-	response.write("require(");
-	response.write("[");
-			response.write("'sampleapp/appui/sampleapp'");
-	response.write("],");
-
-			response.write("function(SampleApp) {");
-					response.write("require.ready(function() {");
-						response.write("function onReady() {");
-							response.write("var staticLoadingScreen = document.getElementById('static-loading-screen');");
-							response.write("staticLoadingScreen.parentNode.removeChild(staticLoadingScreen);");
-						response.write("};");
-
-						response.write("new SampleApp(");
-								response.write("document.getElementById('app'),");
-								response.write("'static/style/',");
-								response.write("'static/img/',");
-								response.write("onReady");
-						response.write(");");
-					response.write("});");
-				response.write("}");
-		response.write(");");
-	response.write("</script>");
-	response.write("</body>");
-	response.write("</html>");
-}
-
-http.createServer(function (req, res) {
-	var path = url.parse(req.url).pathname;
-	switch (path)
-	{
-		case '/':
-			res.writeHead(200, {'Content-Type': 'text/html'});
-  			returnResponse(url.parse(req.url), res);
-  			res.end();
-            break;
-
-        default:
-            if (!fs.existsSync(__dirname + path)) {
-              console.log("HTTP 404: "+ path);
-              res.writeHead(404, {'Content-Type': 'text/html'});
-            }
-
-            else if (/\.(css)$/.test(path))
-            {
-                res.writeHead(200, {'Content-Type': 'text/css'});
-                res.write(fs.readFileSync(__dirname + path, 'utf8'));
-            }
-            else if (/\.(js)$/.test(path))
-            {
-                res.writeHead(200, {'Content-Type': 'text/js'});
-                res.write(fs.readFileSync(__dirname + path, 'utf8'));
-            }
-            else{
-                res.contentType = 'image/png';
-                res.write(fs.readFileSync(__dirname + path));
-            }
-
-            res.end();
-
-        break;
-	}
-}).listen(1337);
-console.log('Server running on port 1337');
+app.listen(1337, function () {
+  console.log('Example app listening on port 1337');
+});
