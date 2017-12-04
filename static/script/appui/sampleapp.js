2d66a9d751c79f71d4ad43279349e113839f90c4
Joel Howard <joel.howard@protonmail.com>
2017-12-04 12:17:01 +0000

    no need to override init

--- static/script/appui/sampleapp.js
+++ static/script/appui/sampleapp.js
@@ -30,23 +30,11 @@ define(
   ],
   function (Application, Container) {
     return Application.extend({
-      init: function init (appDiv, styleDir, imgDir, callback) {
-        init.base.call(this, appDiv, styleDir, imgDir, callback)
-
-        var self = this
-
-        // Sets the root widget of the application to be an empty container
-        this._setRootContainer = function () {
-          var container = new Container()
-          container.outputElement = appDiv
-          self.setRootWidget(container)
-        }
-      },
-
       run: function () {
         // Called from run() as we need the framework to be ready beforehand.
-        this._setRootContainer()
-
+        var container = new Container()
+        container.outputElement = this._rootElement
+        this.setRootWidget(container)
         // Create maincontainer and add simple component to it
         this.addComponentContainer('maincontainer', 'sampleapp/appui/components/simple')
       }
