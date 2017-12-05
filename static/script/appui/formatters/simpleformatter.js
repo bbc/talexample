db5f3439cb16328b5edc4cf8622895e1ad8b082a
Joel Howard <joel.howard@protonmail.com>
2017-12-05 16:49:44 +0000

    Formatters are a little redundant

--- static/script/appui/formatters/simpleformatter.js
+++ static/script/appui/formatters/simpleformatter.js
@@ -25,21 +25,22 @@
 define(
   'sampleapp/appui/formatters/simpleformatter',
   [
-    'antie/formatter',
     'antie/widgets/label',
     'antie/widgets/button',
     'antie/widgets/image'
   ],
-  function (Formatter, Label, Button, Image) {
-    return Formatter.extend({
-      format: function (iterator) {
-        var button, item
-        item = iterator.next()
-        button = new Button('fruit' + item.id)
-        button.appendChildWidget(new Image('img-item.id', item.img, { width: 200, height: 200 }))
-        button.appendChildWidget(new Label(item.title))
-        return button
+  function (Label, Button, Image) {
+    return function () {
+      return {
+        format: function (iterator) {
+          var button, item
+          item = iterator.next()
+          button = new Button('fruit' + item.id)
+          button.appendChildWidget(new Image('img-item.id', item.img, { width: 200, height: 200 }))
+          button.appendChildWidget(new Label(item.title))
+          return button
+        }
       }
-    })
+    }
   }
 )
